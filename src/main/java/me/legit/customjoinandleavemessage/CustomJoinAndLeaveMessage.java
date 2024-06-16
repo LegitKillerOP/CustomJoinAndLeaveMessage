fe096d682f006da8c2878fccd44de827435cec01
Legit Killer <91422151+LegitKillerOP@users.noreply.github.com>
2024-06-16 22:12:11 +0530

    Version 1.0

---
+++ src/main/java/me/legit/customjoinandleavemessage/CustomJoinAndLeaveMessage.java
@@ -0,0 +1,17 @@
+package me.legit.customjoinandleavemessage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomJoinAndLeaveMessage extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
